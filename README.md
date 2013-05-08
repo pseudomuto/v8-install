7d5673aab3f2858be57e95a9027bc7b65985aed8
David Muto <david.muto@gmail.com>
2013-05-07 22:35:57 -0400

    Working on a mac so far...

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-v8-install
-==========
+# v8-install
 
 Scripts to install a standalone version of Google's V8 for all users
+
+## Dependencies
+
+Git 
+
+## Installation
+
+Clone the repo and run the following commands....
+
+	$ ./install.sh
+
+## Updating V8
+
+Run the same script to update the shared repo, rebuild and update symlinks if necessary
+
+	$ ./install.sh
+
+## Coming Soon
+
+Windows installation
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e 
+
+SRC_DIR=/usr/local/src
+SOURCE_DIR="$SRC_DIR/v8"
+BIN_DIR=/usr/local/bin
+REPO_ADDR=git://github.com/v8/v8.git
+
+# ensure src dir
+mkdir -p SRC_DIR &>/dev/null
+
+if [ ! -d $SOURCE_DIR ]; then
+	# clone
+	git clone $REPO_ADDR $SOURCE_DIR	
+fi
+
+# compile for native architecture
+cd $SOURCE_DIR
+
+# update
+echo "Updating V8 sources..."
+git pull &>/dev/null
+
+echo "Installing dependencies and compiling for native environment...(this can take a while)"
+make dependencies &>/dev/null
+make native &>/dev/null
+
+# create symlinks to d8 v8
+echo "Updating symlinks..."
+ln -s "$SOURCE_DIR/out/native/d8" "$BIN_DIR/d8" 
+ln -s "$SOURCE_DIR/out/native/shell" "$BIN_DIR/v8" 
+
+echo "Done! d8 and v8 are now available globally."
